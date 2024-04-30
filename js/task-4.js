07ee71ec4e04c468976c257fb13443a611d7f2b2
Vladislav Pysarenko <vlad@pysarenko.com>
2024-04-30 21:22:22 +0200

    first commit

---
+++ js/task-4.js
@@ -0,0 +1,19 @@
+const shipping = [
+  { country: 'China', price: 100 },
+  { country: 'Chile', price: 250 },
+  { country: 'Australia', price: 170 },
+  { country: 'Jamaica', price: 120 },
+];
+
+function getShippingCost(country) {
+  const shipMethod = shipping.find(({ country: name }) => name === country);
+
+  return shipMethod ? `Shipping to ${country} will cost ${shipMethod.price} credits` : `Sorry, there is no delivery to your country`;
+}
+
+console.log(getShippingCost('Australia')); // "Shipping to Australia will cost 170 credits"
+console.log(getShippingCost('Germany')); // "Sorry, there is no delivery to your country"
+console.log(getShippingCost('China')); // "Shipping to China will cost 100 credits"
+console.log(getShippingCost('Chile')); // "Shipping to Chile will cost 250 credits"
+console.log(getShippingCost('Jamaica')); // "Shipping to Jamaica will cost 120 credits"
+console.log(getShippingCost('Sweden')); // "Sorry, there is no delivery to your country"
